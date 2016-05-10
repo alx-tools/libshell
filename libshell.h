f00cf364e884c16480f7faf46e0d399b749ac716
Alexandre Gautier <alexandre.gautier@epitech.eu>
2016-05-09 21:47:09 -0700

    Shell library

--- libshell.h
+++ libshell.h
@@ -1,8 +1,7 @@
-#ifndef _UTILS_H_
-#define _UTILS_H_
+#ifndef _LIBSHELL_H_
+#define _LIBSHELL_H_
 
 char **string_split(const char *str, char separator);
 char *read_line(const int fd);
-char *get_next_line(const int fd);
 
-#endif /* _UTILS_H_ */
+#endif
